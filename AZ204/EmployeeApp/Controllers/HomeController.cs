7f99496e7df49f2f8c72cb168c3385dca46c27cf
Gajendran Machakalai <gajendran.machakalai4@outlook.com>
2025-10-07 18:22:01 +0530

    Added the appsettings in Web App

--- AZ204/EmployeeApp/Controllers/HomeController.cs
+++ AZ204/EmployeeApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using EmployeeApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System.Diagnostics;
 
 namespace EmployeeApp.Controllers
@@ -7,14 +8,22 @@ namespace EmployeeApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MyAppSettings _appSettings;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptions<MyAppSettings> appSettings,
+            IWebHostEnvironment env)
         {
             _logger = logger;
+            _appSettings = appSettings.Value;
+            _env = env;
         }
 
         public IActionResult Index()
         {
+            _logger.LogInformation($"Current environment: {_env.EnvironmentName}");
+            string msg = $"App: {_appSettings.AppName}, Version: {_appSettings.Version}";
+            ViewBag.Message = msg;
             return View();
         }
 
--- AZ204/EmployeeApp/Program.cs
+++ AZ204/EmployeeApp/Program.cs
@@ -1,8 +1,10 @@
+using EmployeeApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.Configure<MyAppSettings>(builder.Configuration.GetSection("MyAppSettings"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
